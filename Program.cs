814d138715b3d1a38db362e1329281ce6110138d
YaDat354 <22520243@gm.uit.edu.vn>
2023-10-04 09:52:02 +0700

    thu

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Sinhvien
+{
+    class Sinhvien
+    {
+        string ten;
+        string ngaysinh;
+        double hocphi;
+
+        public Sinhvien(string a, string b, double c)
+        {
+            this.ten = a;
+            this.ngaysinh = b;
+            this.hocphi = c;
+        }
+        public string getTen()
+        {
+            return ten;
+        }
+        public string getNgaysinh()
+        {
+            return ngaysinh;
+        }
+        public double getHocphi()
+        {
+            return hocphi;
+        }
+    }
+
+    class SinhvienTN : Sinhvien
+    {
+        int luongthang;
+        public SinhvienTN(string ten, string ngaysinh, double hocphi, int luongthang) : base(ten, ngaysinh, hocphi)
+        {
+            this.luongthang = luongthang;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinhvienTN a;
+            a = new SinhvienTN("Ya Dat", "03/05/2004", 16000000, 0);
+        }
+    }
+}
